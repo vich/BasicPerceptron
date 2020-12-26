c0627694ea220d27a5531dc7fe53da8380c0b35b
slavakorenblit <3755214+vich@users.noreply.github.com>
2020-12-27 01:25:11 +0200

    ArrayExt

---
+++ BasicPerceptron/ArrayExt.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace BasicPerceptron
+{
+    public static class ArrayExt
+    {
+        public static T[] GetRow<T>(this T[,] array, int row)
+        {
+            if (!typeof(T).IsPrimitive)
+                throw new InvalidOperationException("Not supported for managed types.");
+
+            if (array == null)
+                throw new ArgumentNullException("array");
+
+            int cols = array.GetUpperBound(1) + 1;
+            T[] result = new T[cols];
+
+            int size;
+
+            if (typeof(T) == typeof(bool))
+                size = 1;
+            else if (typeof(T) == typeof(char))
+                size = 2;
+            else
+                size = Marshal.SizeOf<T>();
+
+            Buffer.BlockCopy(array, row*cols*size, result, 0, cols*size);
+
+            return result;
+        }
+    }
+}
