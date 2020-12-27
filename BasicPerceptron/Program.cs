2619f0edac187ebe263909795762f2b3ad686c43
Slava Korenblit <slavak@codevalue.com>
2020-12-27 10:30:26 +0200

    clean

--- BasicPerceptron/Program.cs
+++ BasicPerceptron/Program.cs
@@ -9,7 +9,7 @@ namespace BasicPerceptron
     {
         private const string TrainFile = "Train.txt";
         private const string TestFile = "Test.txt";
-        private const int Samples = 10000;
+        private const int Samples = 100;
         private const int Neurons = 21;
         private const double LearningRate = 0.1;
         private const int Bias = 1;
@@ -19,36 +19,14 @@ namespace BasicPerceptron
         {
             GenerateSamples();
             var trainedWeights = TrainPerceptron();
-
-
+            
             Console.ReadLine();
         }
 
         private static double[] TrainPerceptron()
         {
-            var trainingSet = File.ReadAllLines(TrainFile);
-
-            var input = new int[Samples, Neurons];
-            var outputs = new int[Samples];
-            for (var index = 0; index < trainingSet.Length; index++)
-            {
-                var splintedLine = trainingSet[index].Split(" ");
-                var strArr = splintedLine[0].ToCharArray();
-                var neuron = Array.ConvertAll(strArr, c => byte.Parse(c.ToString()));
-                for (var i = 0; i < neuron.Length; i++)
-                {
-                    input[index, i] = neuron[i];
-                }
-
-                outputs[index] = byte.Parse(splintedLine[1]);
-            }
-
-            var random = new Random();
-            var weights = new double[Neurons + 1];
-            for (var i = 0; i < Neurons; i++)
-            {
-                weights[i] = random.NextDouble();
-            }
+            var input = ReadData(TrainFile, out var outputs);
+            var weights = RandomWeights(Neurons + 1);
 
             double totalError = 1;
             var iteration = 1;
@@ -80,19 +58,53 @@ namespace BasicPerceptron
             }
 
             Console.WriteLine("Results:");
-            for (var i = 0; i < Neurons; i++)
+            for (var i = 0; i < Samples; i++)
                 Console.WriteLine(CalculateOutput(input.GetRow(i), weights));
 
             return weights;
+        }
+
+        private static double[] RandomWeights(int neurons)
+        {
+            var random = new Random();
+            var weights = new double[neurons];
+            for (var i = 0; i < neurons; i++)
+            {
+                weights[i] = random.NextDouble();
+            }
+
+            return weights;
+        }
+
+        private static int[,] ReadData(string path, out int[] outputs)
+        {
+            var trainingSet = File.ReadAllLines(path);
+
+            var input = new int[Samples, Neurons];
+            outputs = new int[Samples];
+            for (var index = 0; index < trainingSet.Length; index++)
+            {
+                var splintedLine = trainingSet[index].Split(" ");
+                var strArr = splintedLine[0].ToCharArray();
+                var neuron = Array.ConvertAll(strArr, c => byte.Parse(c.ToString()));
+                for (var i = 0; i < neuron.Length; i++)
+                {
+                    input[index, i] = neuron[i];
+                }
+
+                outputs[index] = byte.Parse(splintedLine[1]);
+            }
+
+            return input;
         }
 
         private static void GenerateSamples()
         {
             var samplesGenerator = new SamplesGenerator(Neurons);
 
-            //if (!File.Exists(TrainFile))
+            if (!File.Exists(TrainFile))
                 samplesGenerator.GenerateSet(Samples, TrainFile);
-            //if (!File.Exists(TestFile))
+            if (!File.Exists(TestFile))
                 samplesGenerator.GenerateSet(Samples, TestFile);
         }
 
